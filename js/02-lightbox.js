c4dd294b78f5bb5bade9c14f83b4ffbb31626862
webnovelty <webnoveltystudio@gmail.com>
2022-07-29 00:14:04 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+function createGalleryItems(galleryItems) {
+	return galleryItems.map((galleryItems) => {
+		return `
+		<a class="gallery__item" href = "${galleryItems.original}" >
+				<img class="gallery__image" src="${galleryItems.preview}" alt="${galleryItems.description}" />
+</a>`
+			;
+	}).join('');
+
+}
+
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	gallery.options.captionsData = 'alt';
+	gallery.options.captionDelay = 250;
+});
+
